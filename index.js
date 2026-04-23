5d830541e715206edd5338eac03a742219261d64
Ender <59123442+EnderquakeYT@users.noreply.github.com>
2026-04-23 15:07:17 +0200

    some more anims

--- index.js
+++ index.js
@@ -13,7 +13,21 @@ function bombPercent() {
 
 document.addEventListener("DOMContentLoaded", () => {
     let startButton = document.getElementById("start");
-    startButton.addEventListener("click",()=>{startButton.style.animationName="startClick";startButton.style.animationDuration="250ms";startButton.style.animationTimingFunction="cubic-bezier(0, 0, 0.18, 1.82)";})
+    startButton.addEventListener("click",()=>{
+        switch (Math.floor(Math.random()*3)) {
+            case 0:
+                startButton.style.animationName="startClick1";startButton.style.animationDuration="250ms";startButton.style.animationTimingFunction="cubic-bezier(0, 0, 0.18, 1.82)";
+                break;
+            case 1:
+                startButton.style.animationName="startClick2";startButton.style.animationDuration="1s";startButton.style.animationTimingFunction="cubic-bezier(0,0,0,1)";
+                break;
+            case 2:
+                startButton.style.animationName="startClick2";startButton.style.animationDuration="250ms";startButton.style.animationTimingFunction="cubic-bezier(1,0,1,1)";
+                break;
+            default:
+                break;
+        }
+    })
     playingField = document.getElementById("playingField");
     xWidth = document.getElementById("xWidth");
     yWidth = document.getElementById("yWidth");
