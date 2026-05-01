aff3d45926ad280e9a9c26e32643c7d915ba38c9
MarekK <marek.kunik08@gmail.com>
2026-05-01 10:36:42 +0200

    fixed bug with PWA support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "minesweeper-v1";
+const FILES_TO_CACHE = [
+    "/WebNaWBA/",
+    "/WebNaWBA/index.html",
+    "/WebNaWBA/index.js",
+    "/WebNaWBA/minefield.html",
+    "/WebNaWBA/minefield.js",
+    "/WebNaWBA/manifest.json",
+    "/WebNaWBA/src/output.css",
+    "/WebNaWBA/assets/bomb.png",
+    "/WebNaWBA/assets/flag.png",
+    "/WebNaWBA/assets/dessert_numbers_1.png",
+    "/WebNaWBA/assets/dessert_numbers_2.png",
+    "/WebNaWBA/assets/dessert_numbers_3.png",
+    "/WebNaWBA/assets/dessert_numbers_4.png",
+    "/WebNaWBA/assets/dessert_numbers_5.png",
+    "/WebNaWBA/assets/dessert_numbers_6.png",
+    "/WebNaWBA/assets/dessert_numbers_7.png",
+    "/WebNaWBA/assets/dessert_numbers_8.png",
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
