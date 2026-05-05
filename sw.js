4c15549e8ce7930cf91d3e0dd96021704ded515a
MarekK <marek.kunik08@gmail.com>
2026-05-05 12:32:56 +0200

    Hopefully truly final bug fix with the icons and textures not updating

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "minesweeper-v1";
+const CACHE_NAME = "minesweeper-v3";
 const FILES_TO_CACHE = [
     "/WebNaWBA/",
     "/WebNaWBA/index.html",
@@ -23,6 +23,16 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
     );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+        )
+    );
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
